805cfd3830fb8337ec0f570f9406e924e9d3428f
Ryan Simkoff <rsimkoff@gmail.com>
2021-07-13 10:56:49 -0400

    updating CLI db for employees

--- db/connection.js
+++ db/connection.js
@@ -1,15 +1,21 @@
 const util = require("util");
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  // Your username
-  user: "root",
-  // Your password
-  password: "password",
-  database: "employees"
-});
-
+// const connection = mysql.createConnection("mysql://ws5x7diuuo01brdx:xjy8s605s32qz626@bmlx3df4ma7r1yh4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ejff16igwxg1atba");
+var connection = mysql.createConnection({
+    multipleStatements: true, 
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "current password",
+    database: "employees"
+  });
 connection.connect();
 
 // Setting up connection.query to use promises instead of callbacks
--- db/seed.sql
+++ db/seed.sql
@@ -1,31 +1,29 @@
-INSERT INTO department (dept_name, utilized_budget)
-VALUES ('Accounting', 45000),
-       ('Marketing', 85000),
-       ('Human Resources', 35000),
-       ('Operations', 100000), 
-       ('Repair & Maintenance', 75000),
-       ('Legal Disputes', 65000);
-
+INSERT INTO department (name)
+VALUES ('Sales'),
+    ('Engineering'),
+    ('Finance'),
+    ('Legal');
 INSERT INTO roles (title, salary, department_id) 
-VALUES ('Chief Executive Officer', 250000, 4), 
-       ('Chief Financial Officer', 200000, 1),
-       ('Controller', 150000, 1),  
-       ('Staff Account', 65000, 1),
-       ('Marketing Manager', 75000, 2),
-       ('Human Resource Director', 60000, 3),
-       ('Legal Aid', 35000, 6),
-       ('Repair & Maintenace Director', 85000, 5);
+VALUES  ('Sales Lead', 100000, 1),
+    ('Salesperson', 80000, 1),
+    ('Lead Engineer', 150000, 2),
+    ('Software Engineer', 120000, 2),
+    ('Account Manager', 160000, 3),
+    ('Accountant', 125000, 3),
+    ('Legal Team Lead', 250000, 4),
+    ('Lawyer', 190000, 4);
 
-INSERT INTO employee (first_name, last_name, emp_dept, salary, manager_id, roles_id)
-VALUES ('Ryan', 'Wolfhunter', 'Operations', 85000, NULL, 1),
-       ('Freddy', 'Spaghetti', 'Operations', 85000, NULL, 2), 
-       ('Franklin', 'Gronkowski', 'Accounting', 75000, NULL, 3),
-       ('Lauren', 'Flakers', 'Legal Disputes', 50000, 3, 7),
-       ('Jason', 'Pretzel', 'Accounting', 80000, 2, 3),
-       ('Heather', 'Copperhead', 'Repair & Maintenance', 45000, 1, 8),
-       ('Mike', 'Baker', 'Human Resources', 50000, 2, 6),
-       ('Patrick', 'Jackson', 'Marketing', 60000, 1, 5),
-       ('Kanye', 'Irving', 'Marketing', 65000, 1, 4);
+INSERT INTO employee 
+(first_name, last_name, role_id, manager_id)
+VALUES ('Ryan', 'Wolfhunter', 1, NULL),
+       ('Freddy', 'Spaghetti', 2, 1), 
+       ('Franklin', 'Gronkowski', 'G', 3, NULL),
+       ('Lauren', 'Flakers', 4, 3),
+       ('Jason', 'Pretzel', 5, NULL),
+       ('Heather', 'Copperhead', 6, 5),
+       ('Mike', 'Baker', 4, 3),
+       ('Patrick', 'Jackson', 5, NULL),
+       ('Kanye', 'Irving', 6, 5);
     
 INSERT INTO manager (id, mgr_name)
 VALUES (1, 'Ryan Wolfhunter'),
